879feb133c5569bc1f5e09c6005c34dfb24bed01
ayazzxcs <droptrendmail@gmail.com>
2026-06-28 22:19:37 +0530

    Split Quvirl brand into white Qu and pink virl

--- patch-pink-brand-name.js
+++ patch-pink-brand-name.js
@@ -2,19 +2,33 @@ const fs = require('fs');
 const file = 'index.html';
 let html = fs.readFileSync(file, 'utf8');
 
+// Split visible Quvirl brand into Qu + virl where possible.
+html = html.replace(/<b>Quvirl<\/b>/g, '<b><span class="brandQu">Qu</span><span class="brandVirl">virl</span></b>');
+html = html.replace(/>(Quvirl)<\/g, '><span class="brandQu">Qu</span><span class="brandVirl">virl</span><');
+
 const css = `
-/* Quvirl pink brand name */
-.brandText b,.miniBrand,.miniBrand span,.sideBrand,.sideBrand span,.brandTrend,.brandWord{
+/* Quvirl split brand name: Qu white, virl pink */
+.brandText b,.miniBrand,.sideBrand,.brandTrend,.brandWord{
+  color:#ffffff!important;
+  -webkit-text-fill-color:#ffffff!important;
+}
+.brandQu{
+  color:#ffffff!important;
+  -webkit-text-fill-color:#ffffff!important;
+}
+.brandVirl{
   color:#ff4fd8!important;
   -webkit-text-fill-color:#ff4fd8!important;
+  text-shadow:0 0 18px rgba(255,79,216,.38)!important;
 }
 .brandText b{
-  text-shadow:0 0 18px rgba(255,79,216,.35)!important;
+  text-shadow:none!important;
 }
 `;
 
 html = html.replace(/\/\* Quvirl pink brand name \*\/[\s\S]*?(?=<\/style>)/g, '');
+html = html.replace(/\/\* Quvirl split brand name: Qu white, virl pink \*\/[\s\S]*?(?=<\/style>)/g, '');
 html = html.replace('</style>', css + '\n</style>');
 
 fs.writeFileSync(file, html, 'utf8');
-console.log('Applied pink Quvirl brand name.');
+console.log('Applied split Quvirl brand name: Qu white, virl pink.');
